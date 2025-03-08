85129b7be753e9836c3d9f4444d0f68e257fcb93
tomas-vo <venegas.osses.tomas@gmail.com>
2025-03-08 18:49:13 -0300

    List of numbers

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,83 @@
 using System;
+using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int addnumber;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        
+        do
+        {
+            Console.WriteLine("Enter number: ");
+            addnumber = int.Parse(Console.ReadLine());
+
+            if (addnumber != 0) 
+            {
+                numbers.Add(addnumber);
+            }
+
+        } while (addnumber != 0); 
+
+        
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers were entered.");
+            return; 
+        }
+
+        int sum = 0;
+        int largestNumber = numbers[0]; 
+
+        
+        foreach (int number in numbers)
+        {
+            sum += number; 
+            if (number > largestNumber) 
+            {
+                largestNumber = number;
+            }
+        }
+
+        
+        double average = (double)sum / numbers.Count;
+
+        
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Average: " + average);
+        Console.WriteLine("Largest Number: " + largestNumber);
     }
 }
+
+
+
+
+/* 
+
+ Enter a list of numbers, type 0 when finished.
+  Enter number: 18
+  Enter number: 36
+  Enter number: 2
+  Enter number: 48
+  Enter number: 6
+  Enter number: 12
+  Enter number: 9
+  Enter number: 0
+  The sum is: 131
+  The average is: 18.714285714285715
+  The largest number is: 48
+
+-crear lista. (hacer bucle preguntando por numeros para add hasta digitar 0)
+
+-hacer total
+-hacer un average
+-encontrar el numero mas alto
+(comparando cada numero de la lista con el anterior. haciendo un index)
+
+*/
