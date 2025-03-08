451bb64e33e1b87081bc6db438758e220dd4d4ee
tomas-vo <venegas.osses.tomas@gmail.com>
2025-03-08 17:43:13 -0300

    Magic Number Exercise :)

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
-}
+        Console.WriteLine("What is the magic number?: ");
+        String magic_number = Console.ReadLine();
+        int number = int.Parse(magic_number);
+
+        int NGuess = 0;
+
+    
+            while (NGuess != number) 
+         {   
+
+            Console.WriteLine(" What is your guess?: ");
+            string Guess = Console.ReadLine();
+            NGuess = int.Parse(Guess);
+
+
+
+            if (NGuess > number )
+            {
+                Console.WriteLine($"Lower");
+            }
+
+            else if (NGuess < number )
+            {
+                Console.WriteLine($"Higher");
+            }
+
+            else
+            {
+                Console.WriteLine($"Correct! the magic number is: {NGuess}");
+            }
+        }
+     }
+}    
+
+// Everything ready :)
